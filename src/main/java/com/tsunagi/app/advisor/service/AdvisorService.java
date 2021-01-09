d7572c83e779ecfb3514d1734c15049e7a8a63bd
twan65 <cx.twan65@gmail.com>
2021-01-10 00:31:38 +0900

    アドバイザー一覧取得ロジックを変更

--- src/main/java/com/tsunagi/app/advisor/service/AdvisorService.java
+++ src/main/java/com/tsunagi/app/advisor/service/AdvisorService.java
@@ -3,6 +3,7 @@ package com.tsunagi.app.advisor.service;
 import com.tsunagi.app.advisor.dto.AdvisorDto;
 import com.tsunagi.app.advisor.dto.AdvisorListRequestDto;
 import com.tsunagi.app.advisor.dto.AdvisorListResponseDto;
+import com.tsunagi.app.domain.advisor.Advisor;
 import com.tsunagi.app.domain.advisor.AdvisorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,13 +30,26 @@ public class AdvisorService {
    */
   @Transactional(readOnly = true)
   public AdvisorListResponseDto findAllBy(AdvisorListRequestDto requestDto) {
-    List<AdvisorDto> advisors =
-        advisorRepository
-                .findAll(requestDto)
-                .stream()
-                .map(AdvisorDto::new)
-                .collect(Collectors.toList());
 
-    return new AdvisorListResponseDto(advisors);
+    List<Advisor> advisors;
+
+    // TODO: ソートはどうするのか検討が必要、検討後に適用しよう。
+    if (requestDto.getExpertise() != null && requestDto.getGender() != null) {
+      advisors = advisorRepository.findByExpertiseAndGenderIsDeletedFalse(requestDto.getExpertise(), requestDto.getGender());
+    } else if (requestDto.getExpertise() != null) {
+      advisors = advisorRepository.findByExpertiseAndIsDeletedFalse(requestDto.getExpertise());
+    } else if (requestDto.getGender() != null) {
+      advisors = advisorRepository.findByGenderAndIsDeletedFalse(requestDto.getGender());
+    } else {
+      advisors = advisorRepository.findByIsDeletedFalse();
+    }
+
+    // EntityリストからDtoリストに変換する。
+    List<AdvisorDto> AdvisorDtoList =
+            advisors.stream()
+            .map(AdvisorDto::new)
+            .collect(Collectors.toList());
+
+    return new AdvisorListResponseDto(AdvisorDtoList);
   }
 }
