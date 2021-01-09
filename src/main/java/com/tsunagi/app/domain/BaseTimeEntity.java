f128df94a223d9a53871c69b4d80a1139f28cbdf
twan65 <cx.twan65@gmail.com>
2021-01-09 17:50:12 +0900

    BaseTimeEntity追加

---
+++ src/main/java/com/tsunagi/app/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.tsunagi.app.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 本クラスのフォール度をカラムとして扱う。（継承したとき）
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
