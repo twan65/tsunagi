359a523583e5993da5dc593a52e17172691c7f1c
twan65 <cx.twan65@gmail.com>
2021-01-09 18:37:46 +0900

    Age→Genderに変更

---
+++ src/main/java/com/tsunagi/app/constant/Gender.java
@@ -0,0 +1,12 @@
+package com.tsunagi.app.constant;
+
+public enum Gender {
+  MAN(0),
+  WOMEN(1);
+
+  private int genderCode;
+
+  Gender(int genderCode) {
+    this.genderCode = genderCode;
+  }
+}
